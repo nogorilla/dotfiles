eb2f9074d2506b35fec3ba647e44afded199789b
Gabriel <gabriel.smith01@gmail.com>
2018-11-28 10:33:31 -0500

    removed npx serverless

--- aliases/js.sh
+++ aliases/js.sh
@@ -1,10 +1,4 @@
 # export NPM_TOKEN=''
-alias nps='npx serverless'
-alias npsd='npx serverless deploy -s'
-alias npsdf='npx serverless deploy function -f'
-alias npsi='npx serverless invoke -f'
-alias npsil='npx serverless invoke local -f'
-alias npsl='npx serverless logs -t -f'
 
 export NVM_DIR="/Users/gabe/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
